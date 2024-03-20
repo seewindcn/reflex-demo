b87b786fa63a8111953f8a8267759b79df8f444c
fulanxiong <fulanxiong@ewan.cn>
2024-03-20 17:40:53 +0800

    update grid_layout

--- demo/demo/components/grid_layout.py
+++ demo/demo/components/grid_layout.py
@@ -1,10 +1,11 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 from functools import lru_cache
 
 import reflex as rx
 from reflex import Component, Var, NoSSRComponent
 
 from reflex.components.el import Div
+from reflex.utils import imports
 
 
 class GridLayoutBase(NoSSRComponent):
@@ -30,19 +31,39 @@ class GridLayoutBase(NoSSRComponent):
 class GridLayout(GridLayoutBase):
     tag = "GridLayout"
     is_default = True
-    cols: Var[int]
+    cols: Var[int] = 12
     width: Var[int]
-    rowHeight: Var[int]
-    autoSize: Var[bool]
+    row_height: Var[int]
+    auto_size: Var[bool]
 
 
 class ResponsiveGridLayout(GridLayoutBase):
-    tag = "Responsive"
-    alias = "ResponsiveGridLayout"
+    tag = "ResponsiveGridLayout"
+    cols: Var[Dict[str, int]] = dict(lg=12, md=10, sm=6, xs=4, xxs=2)
+    breakpoints: Var[Dict[str, int]] = dict(lg=1200, md=996, sm=768, xs=480, xxs=0)
+    row_height: Var[int]
+    auto_size: Var[bool]
+
+    def _get_imports(self) -> imports.ImportDict:
+        return imports.merge_imports(
+            # super()._get_imports(),
+            {self.library: {imports.ImportVar(tag='WidthProvider'), imports.ImportVar(tag='Responsive'), }},
+        )
+
+    def _get_dynamic_imports(self) -> str:
+        return ""
+
+    def _get_custom_code(self) -> str | None:
+        return "const ResponsiveGridLayout = WidthProvider(Responsive);"
 
 
 class GridLayoutItem(Div):
     data_grid: Var[Dict[str, Any]]
+    is_resizable: Var[bool] = False
+    is_draggable: Var[bool] = False
+    resize_handles: Var[List[str]] = ["se"]
+
+    _rename_props = {'dataGrid': 'data-grid'}
 
 
 grid_layout = GridLayout.create
--- demo/demo/layouts.py
+++ demo/demo/layouts.py
@@ -11,20 +11,25 @@ def layout1(path: str = ""):
                 # contents(*args, **kwargs),
                 grid_layout_item(
                     key="header",
-                    data_grid=dict(x=0, y=0, w=12, h=1, static="true"),
+                    data_grid=dict(x=0, y=0, w=12, h=1, static=True),
+                    class_name="border-2 border-gray-300",
+
                 ),
                 grid_layout_item(
                     key="navbar",
-                    data_grid=dict(x=0, y=1, w=2, h=20, static="true"),
+                    data_grid=dict(x=0, y=1, w=2, h=20, static=False),
+                    class_name="border-2 border-gray-300",
+                    is_resizable=True,
                 ),
                 grid_layout_item(
                     contents(*args, **kwargs),
                     key="body",
-                    data_grid=dict(x=3, y=2, w=10, h=20, static="true"),
+                    data_grid=dict(x=3, y=1, w=10, h=20, static=False),
+                    class_name="border-2 border-gray-300",
                 ),
                 class_name="layout",
                 cols=12,
-                rowHeight=30,
+                row_height=30,
                 width=1200,
                 auto_size=True,
             )
@@ -33,4 +38,38 @@ def layout1(path: str = ""):
     return _layout
 
 
-default_layout = layout1
+def layout2(path: str = ""):
+    from .components.grid_layout import responsive_grid_layout, grid_layout_item
+
+    def _layout(contents: Callable[[], rx.Component]):
+        def _wrapper(*args, **kwargs) -> rx.Component:
+            return responsive_grid_layout(
+                # contents(*args, **kwargs),
+                grid_layout_item(
+                    key="header",
+                    data_grid=dict(x=0, y=0, w=12, h=5, static=True),
+                    class_name="border-2 border-gray-300",
+
+                ),
+                grid_layout_item(
+                    key="navbar",
+                    data_grid=dict(x=0, y=5, w=1, h=40, static=False),
+                    class_name="border-2 border-gray-300",
+                ),
+                grid_layout_item(
+                    contents(*args, **kwargs),
+                    key="body",
+                    data_grid=dict(x=1, y=1, w=11, h=20, static=False),
+                    class_name="border-2 border-gray-300",
+                ),
+                class_name="layout",
+                row_height=10,
+                auto_size=True,
+            )
+        return _wrapper
+
+    return _layout
+
+
+default_layout = layout2
+
